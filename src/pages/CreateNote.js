8c5b13f40d78b44947502519a59c118684e9fc28
duckgocloud <linxiaoting100@gmail.com>
2023-02-26 16:36:55 +0100

    editnote

--- src/pages/CreateNote.js
+++ src/pages/CreateNote.js
@@ -1,14 +1,18 @@
 import { Link } from 'react-router-dom';
-import { IoIoAwrrowBack } from 'react-icons/io';
+import { IoIosArrowBack } from 'react-icons/io';
 
 
 const CreatNote = () => {
     return(
         <section>
-            <header className='create-note__beader'>
-                <Link to='/' className='btn'><IoIoAwrrowBack /></Link>
+            <header className='create-note__header'>
+                <Link to='/' className='btn'><IoIosArrowBack /></Link>
                 <button className='btn lg primary'>Save</button>
             </header>
+            <form className='create-note__form'>
+                <input type='text' placeholder='Title' autoFocus />
+                <textarea rows='28' placeholder='Note details...'></textarea>
+            </form>
         </section>
     )
 }
