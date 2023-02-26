b4e0603337cff53c59f9399d729814ca9b8cf447
duckgocloud <linxiaoting100@gmail.com>
2023-02-26 16:22:12 +0100

    createnote.js

--- src/pages/CreateNote.js
+++ src/pages/CreateNote.js
@@ -1,8 +1,15 @@
-import React from "react";
+import { Link } from 'react-router-dom';
+import { IoIoAwrrowBack } from 'react-icons/io';
+
 
 const CreatNote = () => {
     return(
-        <div>CreatNote</div>
+        <section>
+            <header className='create-note__beader'>
+                <Link to='/' className='btn'><IoIoAwrrowBack /></Link>
+                <button className='btn lg primary'>Save</button>
+            </header>
+        </section>
     )
 }
 
