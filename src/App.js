ce8c3d6f840f9b28174d51015d119120c88ebaa6
duckgocloud <linxiaoting100@gmail.com>
2023-02-26 16:16:03 +0100

    basic interface

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Routes, Router, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Router, Route } from 'react-router-dom';
+import Notes from './pages/Notes';
+import CreateNote from './pages/CreateNote';
+import EditNote from './pages/EditNote';
 
 function App() {
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Routes />} />
-      <Route path='/create-note' element={<CreateNote />} />
-      <Route path='/edit-note/:id' element={<EditNote />} />
-    </Routes>
-    </BrowserRouter>
+    <main id='app'>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Notes />} />
+        <Route path='/create-note' element={<CreateNote />} />
+        <Route path='/edit-note/:id' element={<EditNote />} />
+      </Routes>
+      </BrowserRouter>
+    </main>
   );
 }
 
--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,8 +1,26 @@
 import React from "react";
+import { CiSearch } from 'react-icons/ci';
+import dummyNotes from '../dummy_notes';
+import { BsPlusLg } from 'react-icons/bs';
+import { NoteItem } from '../components/NoteItem';
+import { Link } from "react-router-dom";
 
 const Notes = () => {
     return(
-        <div>Notes</div>
+        <section>
+            <header className="notes__header">
+                <h2>My Notes</h2>
+                <input type='text' autoFocus placeholder='keyword....'></input>
+                <button className="btn"><CiSearch /></button>
+            </header>
+            <div className="notes__container" >
+                {
+                    dummyNotes.map(note => <NoteItem key={note.id} note={note} />)
+                }
+            </div>
+            <Link className="btn add__btn"><BsPlusLg /></Link>
+        </section>
+        
     )
 }
 
