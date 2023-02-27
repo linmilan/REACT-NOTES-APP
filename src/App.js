733c4d8913b94aeb78a2ebbc997a53b0a6d3d0c5
duckgocloud <linxiaoting100@gmail.com>
2023-02-27 14:08:18 +0100

    notes

--- src/App.js
+++ src/App.js
@@ -4,19 +4,22 @@ import { BrowserRouter, Routes, Router, Route } from 'react-router-dom';
 import Notes from './pages/Notes';
 import CreateNote from './pages/CreateNote';
 import EditNote from './pages/EditNote';
+import dummyNotes from './dummy_notes';
+import { useState } from 'react';
 
 function App() {
+const [notes, setNotes] = useState(dummyNotes)
   return (
     <main id='app'>
       <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Notes />} />
+        <Route path='/' element={<Notes notes={notes} />} />
         <Route path='/create-note' element={<CreateNote />} />
         <Route path='/edit-note/:id' element={<EditNote />} />
       </Routes>
       </BrowserRouter>
     </main>
   );
-}
 
+  }
 export default App;
--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,11 +1,10 @@
 import React from "react";
 import { CiSearch } from 'react-icons/ci';
-import dummyNotes from '../dummy_notes';
 import { BsPlusLg } from 'react-icons/bs';
 import { NoteItem } from '../components/NoteItem';
 import { Link } from "react-router-dom";
 
-const Notes = () => {
+const Notes = (notes) => {
     return(
         <section>
             <header className="notes__header">
@@ -15,7 +14,7 @@ const Notes = () => {
             </header>
             <div className="notes__container" >
                 {
-                    dummyNotes.map(note => <NoteItem key={note.id} note={note} />)
+                    notes.map(note => <NoteItem key={note.id} note={note} />)
                 }
             </div>
             <Link className="btn add__btn"><BsPlusLg /></Link>
