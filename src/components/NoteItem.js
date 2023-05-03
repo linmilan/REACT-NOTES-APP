b8bf073e0569ba5679f8d353e7c71be19dc66ab4
duckgocloud <linxiaoting100@gmail.com>
2023-05-03 14:38:19 +0200

    css

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export const NoteItem = ({note})=> {
    
     return(
-        <Link to= {`/edit-note/${note.id}`} className='note' style={{background: '#'+note.color,opacity:0.9}}>
+        <Link to= {`/edit-note/${note.id}`} className='note' style={{background: '#'+note.color}}>
             <h4>{note.title.length > 50 ? (note.title.substr(0, 50)) + '...' : note.title}</h4>
             <p>{note.date}</p>
         </Link>
--- src/pages/useGenerateRandomColor.js
+++ src/pages/useGenerateRandomColor.js
@@ -3,7 +3,7 @@ import {useState} from 'react';
 const useGenerateRandomColor = () => {
 	const [color,setColor] = useState("")
 	const generateColor = () =>{
-		// setColor(Math.random().toString(16).substr(-6));
+		
 		setColor(Math.floor(Math.random()* 0xFFFFFF).toString(16))
 	};
 	return {color,generateColor};
